476120640f34582089bcc17f1259708dd7846136
Suyatno Bimo Asmoro <suyatnoasmoro@gmail.com>
2021-11-25 18:21:35 +0700

    added 0.0.0.0

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT);
+    await fastify.listen(process.env.PORT, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
